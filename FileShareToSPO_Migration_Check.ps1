99d1677806fd5f6e99ee7ab312241951ddca83aa
Ben Whitmore <44303264+byteben@users.noreply.github.com>
2020-08-11 11:07:34 +0100

    Update FileShareToSPO_Migration_Check.ps1

--- FileShareToSPO_Migration_Check.ps1
+++ FileShareToSPO_Migration_Check.ps1
@@ -9,17 +9,23 @@ Version:
 1.0
 
 .SYNOPSIS
-This script is designed to highlight files on a fileshare that will not be able to be moved to SharePoint online using the SharePoint Migration Tool
+This script is designed to highlight files on a fileshare that will not be able to be 
+moved to SharePoint online using the SharePoint Migration Tool
 
 .Parameter Path
 File Path to test
 
 .Parameter Length
 Length of path to test. SharePoint online has a 400 character limit for a file path
-https://support.microsoft.com/en-gb/office/invalid-file-names-and-file-types-in-onedrive-and-sharepoint-64883a5d-228e-48f5-b3d2-eb39e07630fa?ui=en-us&rs=en-gb&ad=gb#invalidfilefoldernames
+https://support.microsoft.com/en-gb/office/invalid-file-names-and-file-types-in-onedrive-
+and-sharepoint-64883a5d-228e-48f5-b3d2-eb39e07630fa?ui=en-us&rs=en-
+gb&ad=gb#invalidfilefoldernames
+
+.Parameter OutDir
+Specify target Out Directory
 
 .Example
-Path_Length.ps1 -Path "D:\Test\Files" -Length "400"
+Path_Length.ps1 -Path "D:\Test\Files" -Length "400" -OutDir "C:\Outs"
 
 #>
 Param
@@ -27,18 +33,34 @@ Param
     [Parameter(Mandatory = $True)]
     [String]$Path,
     [Parameter(Mandatory = $False)]
-    [String]$Length = 400
+    [String]$Length = 400,
+    [String]$OutDir
 )
+
+$ScriptPath = $MyInvocation.MyCommand.Path
+$CurrentDir = Split-Path $ScriptPath
 
 #Reset Warning
 $WarningPath = $Null
 
-#Invalid Characters
-#$Exclude = @('"', ',', '*', ':', '<', '>', '?', '/', '\', '|')
+#Specify Out File
+$OutFile = "SPO_Migrate_Out.csv"
 
-
+If ($OutDir) {
+    $OutDir = $OutDir
+}
+Else {
+    $OutDir = $CurrentDir
+}
 #Get file info from fileshare
-$Files = Get-ChildItem -Path $Path -recurse | Select-Object Name, Fullname
+
+Try {
+    $Files = Get-ChildItem -Path $Path -recurse -ErrorAction Continue -ErrorVariable Error_GCI | Select-Object Name, Fullname 
+}
+Catch {
+    Write-Warning "Path error: $($Path), Error: $($Error_GCI)"
+    $WarningPath = $True
+}
 
 #Write-Warning "File path is greater than $($Length) characters for the following files"
 ForEach ($File in $Files) {
@@ -49,15 +71,15 @@ ForEach ($File in $Files) {
 
 #Show error if files with large paths found
 If ($WarningPath -eq $True) {
-    Write-Warning "Some files in path ""$($Path)"" exceeded the path length of $($Length). See the Out-GridView for more information"
+    Write-Warning "Some files in path ""$($Path)"" exceeded the path length of $($Length) or the path was unreadable. See the Out-GridView for more information"
     $WarningPath = $Null
 }
 
-#Show error if files with invalid characters
-If ($WarningChar -eq $True) {
-    Write-Warning "Some files in path ""$($Path)"" contained an invalid charcter. See the Out-GridView for more information"
-    $WarningChar = $Null
-}
+
+#Save Out to Path being checked
+$Files | Select-Object Fullname, @{Name = "Path_Length"; Expression = { $_.FullName.Length } } | Sort-Object Path_Length -Descending  | Export-Csv (Join-Path $OutDir "SPO_Migration_Check.csv") -Append
 
 #Output results to Grid View
-$Files | Select-Object Fullname, @{Name = "Path_Length"; Expression = { $_.FullName.Length } } | Sort-Object Path_Length -Descending  | Out-GridView
+$Files | Select-Object Fullname, @{Name = "Path_Length"; Expression = { $_.FullName.Length 
+    } 
+} | Sort-Object Path_Length -Descending  | Out-GridView
