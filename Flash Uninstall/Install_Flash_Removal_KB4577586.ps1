68c092ae0e063277bd545cb224ea992947957b3a
Ben Whitmore <byteben@users.noreply.github.com>
2021-01-02 15:34:27 +0000

    Update Install_Flash_Removal_KB4577586.ps1

--- Flash Uninstall/Install_Flash_Removal_KB4577586.ps1
+++ Flash Uninstall/Install_Flash_Removal_KB4577586.ps1
@@ -4,6 +4,7 @@
 	 Created by:   	Ben Whitmore
 	 Organization: 	-
 	 Filename:     	Install_Flash_Removal_KB4577586.ps1
+	 Target System: Windows 10 Only
 ===========================================================================
     
 Version:
@@ -14,4 +15,47 @@ Version:
 $ScriptPath = $MyInvocation.MyCommand.Path
 $CurrentDir = Split-Path $ScriptPath
 
-$PatchVersions = Get-ChildItem $CurrentDir | Where-Object {$_.PSIsContainer} | Foreach-Object {$_.Name}
+#Get OS Release ID
+$OS_ReleaseID = Get-ItemProperty "HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion" | Select-Object -ExpandProperty ReleaseID
+$OS_ProductName = Get-ItemProperty "HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion" | Select-Object -ExpandProperty ProductName
+
+#Get OS Architecture
+$OS_Architecture = Switch (Get-CIMInstance -Namespace "ROOT\CIMV2" -Class "Win32_Processor" | Select-Object -ExpandProperty Architecture) {
+	9 { 'x64-based' }
+	0 { 'x86-based' }
+	5 { 'ARM64-based' }
+}
+
+#Build OS Version String
+$OS_String = ($OS_ProductName -split "\s+" | Select-Object -First 2) -Join ' '
+
+#Build Patch Name String
+$PatchRequired = "Update for Removal of Adobe Flash Player for " + $OS_String + " Version " + $OS_ReleaseID + " for " + $OS_Architecture + " systems (KB4577586)"
+
+#Get Patch Titles
+$PatchNames = Get-ChildItem $CurrentDir | Where-Object { $_.PSIsContainer } | Foreach-Object { $_.Name }
+
+#Check if the patch has been downloaded for the current system
+$PatchFound = $False
+
+Foreach ($Patch in $PatchNames) {
+	If ($Patch -eq $PatchRequired) {
+		$PatchFound = $True
+
+		#Get MSU from correct Directory
+		$MSUPath = Get-ChildItem (Join-Path $CurrentDir $Patch) -Recurse | Where-Object { $_.Extension -eq ".msu" }
+	}
+}
+
+#Patch detection determines outcome
+If ($PatchFound) {
+	Write-Host "Patch found for this system"
+	Write-Host "Patch Required: $($PatchRequired)"
+	Write-Host "Patch Name: $($MSUPath.Name)"
+
+}
+else {
+	Write-Host "Patch not found for this system"
+	Write-Host "Patch Required: $($PatchRequired)"
+	Write-Host "Current System: $($OS_String) $($OS_ReleaseID) $($OS_Architecture) PC"
+}
