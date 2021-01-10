4d8755f2cf8d8601375bde497b486aee6b59bca9
Ben Whitmore <byteben@users.noreply.github.com>
2021-01-10 22:21:26 +0000

    Create Render_XML_Function.ps1

---
+++ Render_XML_Function.ps1
@@ -0,0 +1,10 @@
+function WriteXmlToScreen ([xml]$xml)
+{
+    $StringWriter = New-Object System.IO.StringWriter;
+    $XmlWriter = New-Object System.Xml.XmlTextWriter $StringWriter;
+    $XmlWriter.Formatting = "indented";
+    $xml.WriteTo($XmlWriter);
+    $XmlWriter.Flush();
+    $StringWriter.Flush();
+    Write-Output $StringWriter.ToString();
+}
