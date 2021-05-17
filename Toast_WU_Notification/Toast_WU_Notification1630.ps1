803668aec15532c57a561c3ee9c889e440b18dbd
Ben Whitmore <44303264+byteben@users.noreply.github.com>
2021-05-17 16:23:33 +0100

    .

--- Toast_WU_Notification/Toast_WU_Notification1630.ps1
+++ Toast_WU_Notification/Toast_WU_Notification1630.ps1
@@ -66,6 +66,9 @@ function Display-ToastNotification {
     #$WebClient.DownloadFile("$BadgeImageUri", "$BadgeImage")
     #$WebClient.DownloadFile("$HeroImageUri", "$HeroImage")
 
+    #Force TLS1.2 COnnection
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
     $BadgeImageUri = [Uri]::new([Uri]::new($ImageRepositoryUri), $BadgeImgName).ToString()
     $HeroImageUri = [Uri]::new([Uri]::new($ImageRepositoryUri), $HeroImgName).ToString()
     New-Object uri $BadgeImageUri
@@ -112,7 +115,7 @@ function Display-ToastNotification {
         $New_Task = New-ScheduledTask -Description "Toast_Notification_$($ToastGuid) Task for user notification. Title: $($ToastTitle) :: Event:$($ToastText) :: Source Path: $($ToastPath) " -Action $Task_Action -Principal $Task_Principal -Trigger $Task_Trigger -Settings $Task_Settings
         Register-ScheduledTask -TaskName "Toast_Notification_$($ToastGuid)" -InputObject $New_Task
         #Create Reg key to flag Proactive Remediation as successful
-        New-Item -Path "HKLM:\Software\Microsoft" -Name "!ProactiveRemediations"
+        New-Item -Path "HKLM:\Software\Microsoft" -Name "!ProactiveRemediations" -ErrorAction SilentlyContinue
         New-ItemProperty -Path "HKLM:\Software\Microsoft\!ProactiveRemediations" -Name "20H2NotificationSchTaskCreated1630" -Type DWord -Value 1 -ErrorAction SilentlyContinue
     }
 	
