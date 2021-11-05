8e314df235e7c337a7e6dcd7fc941125bc6cb5de
Ben Whitmore <44303264+byteben@users.noreply.github.com>
2021-11-05 13:20:21 +0000

    Update Get-Geo.ps1

--- Get-Geo.ps1
+++ Get-Geo.ps1
@@ -4,6 +4,8 @@ $PublicIP_URI = 'http://ifconfig.me/ip'
 $GEO_URI = 'http://ipinfo.io/'
 
 Write-Host "--------------Testing GEO URL's--------------" -ForegroundColor Cyan
+Write-Host "` "
+
 function TestURL($URL) {
     Try {
         $Request = [System.Net.WebRequest]::Create($URL)
@@ -34,14 +36,16 @@ If (((TestURL $PublicIP_URI) -eq "OK") -and ((TestURL $GEO_URI) -eq "OK")) {
     $MY_Country = $MY_GEO.country
     $MY_LOC = $MY_GEO.loc
     $MY_ISP = $MY_GEO.org
-    
+    Write-Host "` "
     Write-Host "--------------Geolocation information--------------" -ForegroundColor Cyan
+    Write-Host "` "
     Write-Output "IP Address: $MY_IP"
     Write-Output "City: $MY_City"
     Write-Output "Region: $MY_Region"
     Write-Output "Country: $MY_Country"
     Write-Output "Coordinates: $MY_LOC"
     Write-Output "ISP: $MY_ISP"
+    Write-Host "` "
 }
 else {
     Write-Warning "Could not obtain GEO information. URL(s) inaccessible."
